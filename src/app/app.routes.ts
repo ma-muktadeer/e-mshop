3c21bfc5914849538993b57deb83f9f295730a0b
ma-muktadeer <muktadeer.ma@gmail.com>
2025-10-07 00:33:39 +0600

    app

---
+++ src/app/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { Home } from './home/home';
+
+export const routes: Routes = [
+    {
+        path: '', title: 'Home Page', component: Home,
+    },
+];
---
+++ src/app/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  imports: [IonicModule],
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
+})
+export class Home {
+
+}
